12830a76df6f3c3368233c4cdf731bcb1661f81f
Uttam Raj Giri <uttam.giri@rumsan.com>
2021-07-15 14:20:35 +0545

    adding contents

--- data/topTenHotels.js
+++ data/topTenHotels.js
@@ -24,5 +24,33 @@ module.exports = [{
     "image": "/images/hotels/hyatt.jpg",
     "link": "https://www.hyatt.com/en-US/hotel/nepal/hyatt-regency-kathmandu/kathm",
     "location": "Boudha Taragaon, Boudha, Kathmandu"
+},
+{
+    "id": "005",
+    "title": "Temple Tree Resort & Spa",
+    "image": "/images/hotels/temple-tree-resort.jpg",
+    "link": "https://templetreenepal.com/",
+    "location": "6 Gaurighat, Lakeside, Pokhara"
+},
+{
+    "id": "006",
+    "title": "Shangrila Village Resort",
+    "image": "/images/hotels/hotels-shangrila.jpg",
+    "link": "https://hotelshangrila.com/",
+    "location": "Pokhara 33700 Nepal"
+},
+{
+    "id": "007",
+    "title": "Hotel Yak & Yeti",
+    "image": "/images/hotels/yakandyeti.jpg",
+    "link": "https://www.yakandyeti.com/",
+    "location": "Durbar Marg, Kathmandu "
+},
+{
+    "id": "008",
+    "title": "Hotel Mystic Mountain",
+    "image": "/images/hotels/mystic-mountain.jpg",
+    "link": "https://www.hotelmysticmountain.com/",
+    "location": "Baluwapati Deupur 44812"
 }
 ]
