7f5da051ae68368228956407f70cc80fcbbffe1d
Uttam Raj Giri <uttam.giri@rumsan.com>
2021-07-17 07:45:04 +0545

    changes

--- components/ExploreNepal.js
+++ components/ExploreNepal.js
@@ -57,11 +57,11 @@ class ExploreNepal extends Component {
                             <div className="col-lg-5 p-0">
                                 <div className="services-inner">
                                     <div className="services-section-title">
-                                        <h2>Offering The Best of Services</h2>
+                                        <h2>Discover the Beauty of Nepal</h2>
                                         <div className="bar"></div>
                                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
 
-                                        <img src="/images/women-with-table.png" alt="Women" />
+                                        <img src="/images/boudhanath.png" alt="Women" />
                                     </div>
                                 </div>
                             </div>
